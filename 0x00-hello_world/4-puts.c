0874afbb282701d78a4548e6d86f792509ae3e2f
nwamanna <nwamannazika@gmail.com>
2022-12-08 11:00:27 -0800

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * prints string
+ * using puts()
+ */
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(string);
+
+	return(0);
+}
