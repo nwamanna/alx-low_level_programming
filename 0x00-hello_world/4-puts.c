0d6108adb2ac0fbcfa8d227e6aa345c0759f4012
nwamanna <nwamannazika@gmail.com>
2022-12-08 11:28:07 -0800

    noe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/**
- * prints string
- * using puts()
- */
-int main(void)
+/*libaray*/
+int main(void) /*print string*/
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(string);
 
-	return(0);
+	return (0); /* return sucess*/
+
 }
