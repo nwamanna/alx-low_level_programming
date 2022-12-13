40d7b93408e6776ed93ccabd2a6a08bfecc5f876
nwamanna <nwamannazika@gmail.com>
2022-12-13 11:16:44 -0800

    lo

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,13 @@
 /**
 * _isalpha - tells if an input is an alphabet
 *
-*c: parameter
+*@c: parameter
 *Return: 1 if alphabet
 */
 
 int _isalpha(int c) /* c parameter */
 {
-	if ((c > 64) || (c < 91))
-		return (1);
-	else if ((c > 96) || (c < 123))
+	if (((c > 64) && (c < 91)) || ((c > 96) || (c < 123)))
 		return (1);
 	else
 		return (0);
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -5,5 +5,7 @@ int main(void)
 	r = _isalpha(108);
 
     	_putchar(r + '0');
+	r= _isalpha('9');
+	_putchar(r + '0');
 	return (0);
 }
