c873e96a1c39c67d27b3abeea3de3154494775ac
nwamanna <nwamannazika@gmail.com>
2022-12-13 12:38:54 -0800

    jj

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_sign - print sign
+*@n: parameter
+*
+*Return: 1, -1, 0
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(43);
+		return (1);
+	}
+}
+
