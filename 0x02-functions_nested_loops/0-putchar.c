03dcfb3a423bec15b5ec1c40c0578a3db7300f39
nwamanna <nwamannazika@gmail.com>
2022-12-13 07:36:56 -0800

    edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,13 @@
 /**
 * main - prints all characters passed to the function
 *
-* Return - 0 if suceesful
+* Return: 0 if suceesful
 */
 
 int main(void)
-{   
+{
 	_putchar(95, 112, 117, 116);
 	_putchar(99, 104, 97, 114);
 	putchar('\n');
-
-    
-    return (0);
+	return (0);
 }
