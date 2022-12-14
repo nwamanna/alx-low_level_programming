472340e44954f43486fafc2fbdea088bd96358e2
nwamanna <nwamannazika@gmail.com>
2022-12-13 19:08:47 -0800

    add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,22 @@
 */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (; n < 99; n++)
 			printf("%d, ", n);
+			if (n == 98)
+				break;
+			printf(",");
+			printf(" ");
 	}
 	else
 	{
-		for (; n > 99; n--)
-			printf("%d, ", n);
+		for (; n > 97; n--)
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(",");
+			printf(" ");
 	}
 }
