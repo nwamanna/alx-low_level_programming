f52b69f21ee2826a236b3b73ff8e32c0b08363c1
nwamanna <nwamannazika@gmail.com>
2022-12-13 19:00:10 -0800

    bety

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+*print_to_98 - print numbers
+*@n: parameter
+*
+*Return: void
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n < 99; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (; n > 99; n--)
+			printf("%d, ", n);
+	}
+}
