c23594688590fb80c819827c28af205db093dc34
nwamanna <nwamannazika@gmail.com>
2022-12-15 05:02:10 -0800

    multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*mul - multiplies two integer
+*@a: first parameter
+*@b: second parameter
+*
+*Return: 0 if successful
+*/
+int mul(int a, int b)
+{
+	int j = a * b;
+
+	printf("%d", j);
+	return (j);
+}
