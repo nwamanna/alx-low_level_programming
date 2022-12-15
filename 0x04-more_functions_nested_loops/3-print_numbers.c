f88ecba142866783c2bcf46aabca5e8327c23dd8
nwamanna <nwamannazika@gmail.com>
2022-12-15 07:01:19 -0800

    function fixed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*print_numbers -This function prints natural numbers
+*
+*Return: 0 if successful
+*/
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
