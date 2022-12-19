c5d086b0818595c6f774c52f710d618b14ef6ac7
Nwamanna <nwamannazika@gmail.com>
2022-12-19 15:58:42 +0100

    switch

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - resets interger value to 98
+ * @*n: pointer to an integer
+ * 
+ * Return: Void
+ */
+
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a project on pointer, arrays and strings
