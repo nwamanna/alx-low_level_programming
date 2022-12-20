7cb07053a9fe1aaa852990edcbb04b322aefc301
Nwamanna <nwamannazika@gmail.com>
2022-12-20 03:20:02 +0100

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* *_strcpy - copy string
+*@src: pointer
+*@dest: pointer
+*
+*Return: the pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int k;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	for (k = 0; k <= length; k++)
+	{
+		dest[k] = src[k];
+	}
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
