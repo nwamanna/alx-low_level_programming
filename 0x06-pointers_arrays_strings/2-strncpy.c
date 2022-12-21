493afa6c120737e1f7451acffae70be228ea62fd
Nwamanna <nwamannazika@gmail.com>
2022-12-21 21:34:28 +0100

    bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_strncpy - copy one string
+*@dest: first parameter
+*@src: second parameter
+*@n: integer
+*
+*Return: dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-    int len = strlen(dest);
-    int length = strlen(src);
-    int i = 0;
-    
-    while (i < n && *src)
-    {
-        dest[i] = *src;
-        *src++;
-        i++;
-    }
-    if (i == length)
-        dest[i] = '\0';
-    else
-        dest[len] = '\0';
-    return (dest);
+	int len = strlen(dest);
+	int length = strlen(src);
+	int i = 0;
+
+	while (i < n && *src)
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+	if (i == length)
+		dest[i] = '\0';
+	else
+		dest[len] = '\0';
+	return (dest);
 }
