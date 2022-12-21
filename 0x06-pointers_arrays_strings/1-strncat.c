a5b1d9fb70d731bdd19b30b4871b2b262ce65efe
Nwamanna <nwamannazika@gmail.com>
2022-12-21 21:10:18 +0100

    bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* _strncat - concatates srting equal to n bytes
+*@dest: first string
+*@src: second string
+*@n: number parameter
+*
+*Return: dest
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-    int len = strlen(dest);
-    int i = 0;
-    
-    while (i < n && *src)
-    {
-        dest[len + i] = *src;
-        *src++;
-        i++;
-    }
-    dest[len + i] = '\0';
-    return (dest);
+	int len = strlen(dest);
+	int i = 0;
+
+	while (i < n && *src)
+	{
+		dest[len + i] = *src;
+		*src++;
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
 }
