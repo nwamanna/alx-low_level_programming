01729def2c250c1a9bdf7dbc5c6032b836e3076c
Nwamanna <nwamannazika@gmail.com>
2022-12-22 11:24:47 +0100

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcmp - compare strings
+*@s1: first string
+*@s2: second string
+*
+*Return: int
+*/
 int _strcmp(char *s1, char *s2)
 {
-   int len = strlen(s1);
-   int i = 0;
-   
-    int k = s1[i];
-    int l = s2[i];
-    int diff = k - l;
-   return(diff);
+	int len = strlen(s1);
+	int i = 0;
+
+	int k = s1[i];
+	int l = s2[i];
+	int diff = k - l;
+
+	return (diff);
 }
