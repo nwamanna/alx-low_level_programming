7593bff55c9d310db3ddbac60494bd29c43e2aef
Nwamanna <nwamannazika@gmail.com>
2022-12-22 15:45:32 +0100

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* string_toupper - conerts lower case to upper
+*@s: string to convert
+*
+*Return: s
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
