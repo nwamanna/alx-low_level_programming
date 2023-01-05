a97b8d55ea86befa564f87b2395166a6020ae064
Nwamanna <nwamannazika@gmail.com>
2023-01-05 22:57:20 +0100

    lenght added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen_recursion - counts number of letters in a string
+*@s: the pointer to the string
+*
+*Return: integer, number of lettersin a string
+*/
+int _strlen_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1);
+}
