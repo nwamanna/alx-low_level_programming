9609cbc5902d5d491c406504d57880cd0ca1e5a5
Nwamanna <nwamannazika@gmail.com>
2023-01-09 22:03:31 +0100

    malloc memory allocated

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+*create_array - creates and array of chars and initilaizes it with a specific char
+*@size: parameter to be multiplied to get size of bytes
+@c: character
+*
+*Return: pointer to array or null if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *arr = (char*) malloc(sizeof(c) * size);
+
+	if (arr ==  NULL)
+	{
+		printf("cant allocate %ld bytes after %d calls\n", sizeof(c) * size, i);
+		return (NULL);
+	}
+	for (i = 0; i < (sizeof(c) * size); i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}	
