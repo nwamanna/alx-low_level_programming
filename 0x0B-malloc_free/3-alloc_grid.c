0008547526c91098896ccbb2ef46fa7407a41c0f
Nwamanna <nwamannazika@gmail.com>
2023-01-11 04:02:19 +0100

    memory allocated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *) malloc(sizeof(width) * width);
+		arr[i] = (int *) malloc(sizeof(width));
 
 		for (j = 0; j < width; j++)
 		{
