222697aeca31fa3b770b2f5146f9cdc71a311467
Nwamanna <nwamannazika@gmail.com>
2023-01-11 04:30:03 +0100

    error fixed

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+*free_grid - frees memory allocated to a 2 dimensional array
+*@grid: pointer to 2 dimensional array
+*@height: columns
+*
+*Return: Void
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int total_length = 0;
+	char *concatenated_string = malloc(total_length * sizeof(char));
+	int current_index = 0;
+
+    if (ac == 0 || av == NULL)
+	{
+        return NULL;
+    }
+    for (int i = 0; i < ac; i++)
+	{
+        total_length += strlen(av[i]) + 1;
+    }
+	if (concatenated_string == NULL)
+	{
+        return NULL;
+    }
+	for (int i = 0; i < ac; i++)
+	{
+        for (int j = 0; av[i][j] != '\0'; j++)
+		{
+            concatenated_string[current_index++] = av[i][j];
+        }
+        concatenated_string[current_index++] = '\n';
+    }
+
+    return concatenated_string;
+}
