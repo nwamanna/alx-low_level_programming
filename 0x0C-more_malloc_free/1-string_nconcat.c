40a400b88d1b7e9faffeb38fea1c71a27d88fbb5
Nwamanna <nwamannazika@gmail.com>
2023-01-12 20:59:21 +0100

    memory fixed

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*string_nconcat - concatates n amount of bytes to s1 
+*@s1: first string
+*@s2: string to be concatated
+*
+*Return: pointer to newly allocated memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+	char *p = malloc(len1 + n + 1);
+
+	if (n >= len2)
+	{
+		n = strlen(s2);
+	}
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(p, s1);
+	strncpy(p + len1, s2, n);
+	p[len1 + n] = '\0';
+	return (p);
+}
