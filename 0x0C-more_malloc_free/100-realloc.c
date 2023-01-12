365cc1da8d1d3c3eb06dccc9486ae40cc9ad032d
Nwamanna <nwamannazika@gmail.com>
2023-01-12 22:57:38 +0100

    memory reallocated

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*_realloc - reallocates memory
+*ptr: pointer to old memory size
+*@old_size: old memory size
+*@new_size: new memory size
+*
+*Return: Void
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+{
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+}
+
+
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*array_range - create an array
+*@min: lowest parameter
+*@max: highest parameter
+*
+*Return: integer pointer
+*/
+int *array_range(int min, int max)
+{
+	int i;
+	int cal = (max - min) + 1;
+	int *p = malloc(cal * sizeof(int));
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= cal; i++)
+	{
+		p[i] = min + i;
+	}
+	return (p);
+}
+
+
