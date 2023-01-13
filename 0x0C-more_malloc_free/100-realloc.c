816c2181998d051565d32d7e31b1e740466a47d1
Nwamanna <nwamannazika@gmail.com>
2023-01-13 22:58:53 +0100

    memory allocated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,19 +11,42 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *newptr;
+	unsigned int i;
+
 	if (new_size == old_size)
 	{
 		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+
+		if (newptr == NULL)
+		{
+			return (NULL);
+	}
+	if (new_size > old_size && ptr != NULL)
+	{
+		newptr = malloc(new size);
+	
+	if (newptr == NULL)
+	{
+		return (newptr);
+	}
+	for (i = 0; i < old_size; i++)
+	{
+		newptr[i] = *(char *)ptr + 1);
+		free(ptr);
+	}
+	return (newptr
+}
+
 }
 
 
