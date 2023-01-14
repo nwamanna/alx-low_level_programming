2c90ef16e4f88373424191f6f4321e50ec73025a
Nwamanna <nwamannazika@gmail.com>
2023-01-14 14:05:51 +0100

    memory allocated and deallocated

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+*main - entry point
+*@argc: argument count
+*@argv: argument vector
+*
+*Return: int 0 for success
+*/
+int main(int argc, char **argv)
+{
+	int i;
+	int *p;
+
+	if (!(argc == 3) || argc > 3)
+	{
+		printf("error\n");
+		exit(98);
+	}
+	p = (int *) malloc(sizeof(int));
+
+	for (i = 0; i < argc; i++)
+	{
+		if (isalpha(*argv[i]))
+		{
+			printf("error\n");
+			exit(98);
+		}
+	}
+	*p = atoi(argv[1]) * atoi(argv[2]);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	printf("%d\n", *p);
+	free(p);
+	return (0);
+}
