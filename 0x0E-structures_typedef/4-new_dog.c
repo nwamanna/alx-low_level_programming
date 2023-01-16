8d222bf2aa531fbbea6fc3a20e9fc03b768508d8
Nwamanna <nwamannazika@gmail.com>
2023-01-16 19:59:51 +0100

    memory deallocated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,16 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	dog_t *d;
+	char a, b;
 
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
-	return (ptr);
+	d->name = name;
+	a = d->name;
+	d->age = age;
+	d->owner = owner;
+	b = d->owner;
+	return (d);
 }
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+*free_dog - function to create structure dog varaible
+*@d: pointer of type dog_t
+*
+*Return: dog_t
+*/
+void free_dog(dog_t *d);
+{
+	free(d);
+}
