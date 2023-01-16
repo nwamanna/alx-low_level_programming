79aff2f0b83925556e735bb3f980fa2db76c072d
Nwamanna <nwamannazika@gmail.com>
2023-01-16 18:19:42 +0100

    structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+This project is about structures and typedef.
+Structures contain data of multiple types unlike arrays that contain data of a single type.
+typedef is used to name an alias of data type, to make for more readability of code
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+*struct dog - this structure contains details on name, age and the owner of a dog 
+*@name: name of dog
+*@age: age of dog
+*@owner: name of dog owner
+*
+*Description: name and owner are pointers with type char, age is an integer
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
