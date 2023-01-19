b0b10a6c0ba34f0a9eac4111790a0cd44fe98bc3
Nwamanna <nwamannazika@gmail.com>
2023-01-19 07:58:43 +0100

    function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
