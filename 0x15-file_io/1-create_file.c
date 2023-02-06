a10be7dffb02f5b0fd03145e6cc4baa09ce07e35
Nwamanna <nwamannazika@gmail.com>
2023-02-06 21:53:41 +0100

    file handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,30 @@
 #include "main.h"
+/**
+*create_file _ create file
+*@filename: name of file
+*@text_content: content to be written
+*
+*Return: 1 on success -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
 	int fd, fd2;
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_EXCL|O_CREAT|O_RDWR|O_TRUNC, S_IWUSR|S_IRUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
 	fd2 = write(fd, text_content, strlen(text_content));
-	return(fd2);
+	if (fd2 < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return(1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,26 @@
 #include "main.h"
+/**
+*append_text_to_file - append text to file
+*@filename: name of file
+*text_content
+*Return: 1 on success -1 o error
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, fd2;
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_WRONLY|O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return 1;
 	fd2 = write(fd, text_content, strlen(text_content));
-	printf("%d", fd2);
+	if (fd2 < 0)
+	{
+		close(fd);
+		return -1;
+	}
 	close(fd);
-	return(fd2);
+	return(1);
 }
