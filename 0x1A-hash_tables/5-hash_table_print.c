d008c57808baefcd652fd684f6f3ccd318ddc4da
Nwamanna <nwamannazika@gmail.com>
2023-04-07 21:28:04 +0100

    print key/value function

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,37 @@
 #include "hash_tables.h"
 /**
+*hash_table_count - counts keys in the table
+*@ht: hash table
+*
+*Return: count
+*/
+int hash_table_count(const hash_table_t *ht)
+{
+	int count = 0;
+	unsigned long int i;
+	hash_node_t *ptr;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		exit(98);
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		if (ptr == NULL)
+			continue;
+		count++;
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
 *hash_table_print - print keys and values in a hash_table
 *@ht: hash_table
 *
@@ -17,16 +49,15 @@ void hash_table_print(const hash_table_t *ht)
 		exit(98);
 	}
 	printf("{");
+	count = hash_table_count(ht);
 	for (i = 0; i < ht->size; i++)
 	{
 		ptr = ht->array[i];
 		if (ptr == NULL)
 			continue;
-		count++;
 		while (ptr->next != NULL)
 		{
 			ptr = ptr->next;
-			count++;
 		}
 	}
 	for (i = 0; i < ht->size; i++)
